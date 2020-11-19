1c739c1d25dcabc842c937ec6b37e3dc84d66a80
Reconcyl <22424430+Reconcyl@users.noreply.github.com>
2020-11-19 00:49:23 -0800

    make error messages more consistent

--- rjam/src/main.rs
+++ rjam/src/main.rs
@@ -8,7 +8,7 @@ mod utils;
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 2 {
-        eprintln!("Please pass the program as an argument.");
+        eprintln!("error: please pass the program as an argument");
         return;
     }
     let bytecode = match parse::parse(args[1].as_bytes()) {
@@ -17,7 +17,7 @@ fn main() {
     };
     let mut runtime = runtime::Runtime::new();
     if let Err(e) = runtime.run(&bytecode) {
-        eprintln!("error: {}", e);
+        eprintln!("runtime error: {}", e);
     }
     runtime.print_stack();
 }
